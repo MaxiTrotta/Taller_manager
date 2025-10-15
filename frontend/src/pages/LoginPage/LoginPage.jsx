0de0112a7f1aae4543ef8c740c6ef8a758dac72d
MaxiTrotta <maxiprogramacion372@gmail.com>
2025-10-15 18:43:40 -0300

    fix video de login

--- frontend/src/pages/LoginPage/LoginPage.jsx
+++ frontend/src/pages/LoginPage/LoginPage.jsx
@@ -62,39 +62,45 @@ export function LoginPage() {
                 <div className={classes.videoWrapper}>
                     <LiteYouTubeEmbed
                         id="BkBkN5-x3ss"
-                        title="Talleres Ortiz Institucional 2023"
-                        alwaysLoadIframe
+                        title="Video de bienvenida a Ortiz Hnos"
+                        poster="maxresdefault"
                         autoplay
                         muted
+                        alwaysLoadIframe
+                        noCookie
+                        showinfo="0"
+                        params="autoplay=1&mute=1&loop=1&playlist=BkBkN5-x3ss&controls=0&showinfo=0&modestbranding=1&rel=0"
+                        iframeClass={classes.videoIframe}
                     />
+
                 </div>
 
                 <div className={classes.formWrapper}>
                     <Paper className={classes.form} radius="lg">
-                    <Title order={2} className={classes.title}>
-                        Bienvenido a Ortiz Hnos!
-                    </Title>
+                        <Title order={2} className={classes.title}>
+                            Bienvenido a Ortiz Hnos!
+                        </Title>
 
-                    <TextInput label="Email" placeholder="usuario@gmail.com"
-                        size="md" radius="md"
-                        error={form.formState.errors.email?.message}
-                        {...form.register("email")}
-                    />
-                    <PasswordInput label="Contraseña" placeholder="Contraseña"
-                        mt="md" size="md" radius="md"
-                        error={form.formState.errors.password?.message}
-                        {...form.register("password")}
-                    />
-                    {error ? <p className="errorMessage">{error}</p> : null}
+                        <TextInput label="Email" placeholder="usuario@gmail.com"
+                            size="md" radius="md"
+                            error={form.formState.errors.email?.message}
+                            {...form.register("email")}
+                        />
+                        <PasswordInput label="Contraseña" placeholder="Contraseña"
+                            mt="md" size="md" radius="md"
+                            error={form.formState.errors.password?.message}
+                            {...form.register("password")}
+                        />
+                        {error ? <p className="errorMessage">{error}</p> : null}
 
-                    <Button fullWidth mt="xl" size="md" radius="md"
-                        variant="filled"
-                        onClick={form.handleSubmit(onSubmit)}
-                        loading={form.formState.isSubmitting}>
-                        Iniciar sesión
-                    </Button>
+                        <Button fullWidth mt="xl" size="md" radius="md"
+                            variant="filled"
+                            onClick={form.handleSubmit(onSubmit)}
+                            loading={form.formState.isSubmitting}>
+                            Iniciar sesión
+                        </Button>
 
-                </Paper>
+                    </Paper>
                 </div>
             </div>
         </>
