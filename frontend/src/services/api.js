59e3dc1bc8d4277277ef04246a14e7a0fec7ae78
Maxi Trotta <maxiprogramacion372@gmail.com>
2025-11-17 21:59:10 -0300

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:9091";
+const API_BASE_URL = import.meta.env.VITE_API_URL;
+
 
 export const api = axios.create({
 	baseURL: API_BASE_URL,
