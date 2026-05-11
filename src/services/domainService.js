d0c1bc9458cc66c3092cda901ef3aec5ff0bfdf7
Maxi Trotta <maxiprogramacion372@gmail.com>
2026-05-10 21:30:36 -0300

    Delete src directory

--- src/services/domainService.js
+++
@@ -1,8 +0,0 @@
-import { api } from "./api";
-
-export const domainService = {
-	getAllDomains: () => api.get("/domains"),
-	getDomainById: (id) => api.get("/domains/" + id),
-	createDomain: (data) => api.post("/domains", data),
-	deleteDomainById: (id) => api.delete("/domains" + id),
-};
--- src/services/sectorsService.js
+++
@@ -1,5 +0,0 @@
-import { api } from "./api"; // tu instancia de Axios
-
-export const sectorsService = {
-  getAllSectors: () => api.get("/sectors"), // endpoint que devuelve todos los sectores
-};
